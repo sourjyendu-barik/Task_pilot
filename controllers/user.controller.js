85789f8e080af90c4b23f1d0000333b98c40c8f5
sourjyendu-barik <sourjyendubarik7798@gmail.com>
2025-12-27 17:42:00 +0530

    add get  all user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -68,4 +68,23 @@ const authLogin = async (req, res) => {
   }
 };
 
-module.exports = { addNewUser, authLogin };
+const allUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    return res.status(200).json({
+      success: true,
+      message:
+        users.length > 0
+          ? "Users data fetched successfully"
+          : "Currently there are no users",
+      data: users,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Server error",
+    });
+  }
+};
+
+module.exports = { addNewUser, authLogin, allUsers };
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { addNewUser, authLogin } = require("../controllers/user.controller");
+const {
+  addNewUser,
+  authLogin,
+  allUsers,
+} = require("../controllers/user.controller");
 router.post("/auth/signup", addNewUser);
 router.post("/auth/login", authLogin);
+router.get("/auth/users", allUsers);
 module.exports = router;
