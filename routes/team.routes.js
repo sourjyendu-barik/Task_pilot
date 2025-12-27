2792627bbfb513417c4166fe9b73da98d809d14a
sourjyendu-barik <sourjyendubarik7798@gmail.com>
2025-12-27 13:58:10 +0530

    added additional routes

--- routes/team.routes.js
+++ routes/team.routes.js
@@ -4,10 +4,15 @@ const {
   addTeam,
   getTeam,
   deleteTeam,
+  addMember,
+  deleteMember,
+  allteams,
 } = require("../controllers/teams.controller");
 
 router.post(`/teams`, addTeam);
 router.get(`/teams/:id`, getTeam);
 router.delete(`/teams/:id`, deleteTeam);
-
+router.get(`/teams`, allteams);
+router.post(`/teams/member/:id`, addMember);
+router.delete(`/teams/member/:id`, deleteMember);
 module.exports = router;
